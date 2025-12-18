bf1026d15fb1d5a1a26957b53b7b1776a5e07e60
thmarkou <theofanis.markou@gmail.com>
2025-12-19 01:05:35 +0200

    Simplify _layout.tsx - let Expo Router auto-discover routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,23 +5,9 @@ export default function RootLayout() {
   return (
     <>
       <StatusBar style="light" />
-      <Stack
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#000000',
-          },
-          headerTintColor: '#FFFFFF',
-          headerTitleStyle: {
-            fontWeight: '600',
-          },
-        }}
-      >
-        <Stack.Screen name="index" options={{ title: 'HamLogbook' }} />
-        <Stack.Screen name="log-qso" options={{ title: 'Log QSO' }} />
-        <Stack.Screen name="qso-list" options={{ title: 'QSO Log' }} />
-        <Stack.Screen name="stats" options={{ title: 'Statistics' }} />
+      <Stack>
+        <Stack.Screen name="index" />
       </Stack>
     </>
   );
 }
-
